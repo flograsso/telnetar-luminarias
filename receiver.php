eb261df0c19f772b0642a35fc558419af9d8aff4
Federico Lo Grasso <lograssofede@gmail.com>
2018-07-25 11:53:56 -0300

    a

--- receiver.php
+++ receiver.php
@@ -1,7 +1,5 @@
-
-
 <!DOCTYPE html>
-    <html lang="en" >
+<html lang="en">
 <?php
 header("Content-Type: text/html;charset=utf-8");
 
@@ -21,36 +19,56 @@ $conn = new mysqli($server, $username, $password, $db);
 
 $crudo=json_encode($data);
 
-$deveui=$data["deveui"];
-$dataFrame= bin2hex(base64_decode($data["dataFrame"]));
-$port=$data["port"];
-$fcnt=$data["fcnt"];
-$rssi=$data["rssi"];
-$snr=$data["snr"];
-$sf_used=$data["sf_used"];
-$id=$data["id"];
+if( isset($data["dataFrame"])) //Receive Uplink Payload Callback
+{
+    $deveui=$data["deveui"];
+    $dataFrame= bin2hex(base64_decode($data["dataFrame"]));
+    $port=$data["port"];
+    $fcnt=$data["fcnt"];
+    $rssi=$data["rssi"];
+    $snr=$data["snr"];
+    $sf_used=$data["sf_used"];
+    $id=$data["id"];
+    $live=$data["live"];
+    $timestamp=$data["timestamp"];
 
-$decrypted=$data["decrypted"] ."<br>";
-echo $decrypted ."<br>";
-$live=$data["live"];
-$timestamp=$data["timestamp"];
-
-if ($decrypted == NULL)
-    $decrypted="0";
-else    
-    $decrypted="1";
-
-
-
+    if ($decrypted == NULL)
+        $decrypted="0";
+    else    
+        $decrypted="1";
 
 //$sql="INSERT INTO `orbiwise` (crudo,deveui,payload,port,fcnt,rssi,snr,sf_used,payload_id,decrypted,live,timestamp) VALUES ('$crudo','$deveui','$payload','$port','$fcnt','$rssi','$snr','$sf_used','$payload_id','$decrypted','$live','$timestamp');";
 
 $sql="INSERT INTO `orbiwise` (crudo,deveui,payload,port,fcnt,rssi,snr,sf_used,payload_id,decrypted,live,timestamp) VALUES ('$crudo','$deveui','$dataFrame','$port','$fcnt','$rssi','$snr','$sf_used','$id','$decrypted','$live','$timestamp');";
 
 $conn->query($sql);
+}
+
+if( isset($data["transmissionStatus"])) //Downlink Payload Status Callback
+{
+
+}
+
+if( isset($data["registration_status"])) //Node Info Update Callback
+{
+
+}
+
+if( isset($data["battery_status"])) //Node Status Update Callback
+{
+
+}
+
+if( isset($data["join_accept"])) //Join Callback
+{
+
+}
+
+
+
+
 
 
 
 
 ?>
-
