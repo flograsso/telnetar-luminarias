bf8b1bd4776dd03ad611797d0e86d283df4da38c
Federico Lo Grasso <lograssofede@gmail.com>
2018-07-25 09:49:07 -0300

    a

--- receiver.php
+++ receiver.php
@@ -23,24 +23,11 @@ $datos=json_encode($data);
 
 echo $data["deveui"];
 
-/*
-$sql="INSERT INTO `orbiwise` (crudo,deveui,payload,port,fcnt,rssi,snr,sf_used,payload_id,decrypted,live,timestamp) 
-VALUES (
-'$datos',
-'$data["deveui"]',
-'$data["dataFrame"]',
-'$data["port"]',
-'$data["fcnt"]',
-'$data["rssi"]',
-'$data["snr"]',
-'$data["sf_used"]',
-'$data["id"]',
-'$data["decrypted"]',
-'$data["live"]',
-'$data["timestamp"]');";
+
+$sql="INSERT INTO `orbiwise` (crudo,deveui,payload,port,fcnt,rssi,snr,sf_used,payload_id,decrypted,live,timestamp) VALUES ('$datos','$data["deveui"]','$data["dataFrame"]','$data["port"]','$data["fcnt"]','$data["rssi"]','$data["snr"]','$data["sf_used"]','$data["id"]','$data["decrypted"]','$data["live"]','$data["timestamp"]');";
 
 echo $conn->query($sql);
-*/
+
 
 
 echo json_encode($data);
